266fd7acd45584a195dbba7e5e586030f1742e71
Upadhyay <Somnath.Upadhyay2@emc.com>
2017-12-08 17:03:57 +0530

    spring boot 1.5.9.war demo

---
+++ demo-war/src/main/java/demo/sb159/war/DemoWarApplication.java
@@ -0,0 +1,12 @@
+package demo.sb159.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWarApplication.class, args);
+	}
+}
---
+++ demo-war/src/main/java/demo/sb159/war/controller/HomeController.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package demo.sb159.war.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author upadhs5
+ *
+ */
+@RestController
+public class HomeController {
+
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "Hello welcome to spring boot 1.5.9, war demo";
+	}
+	
+	@PostMapping(path="/hello", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Map<String,String> postHello(@RequestBody String payload){
+		Map<String, String> map = new HashMap<>();
+		map.put("message",payload);
+		return map; 
+	}
+}
